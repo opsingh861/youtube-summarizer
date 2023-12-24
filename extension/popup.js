4bccd1f0889122f8b0de0a16fc845868c38f9d6d
Aditya Dhanraj <opsingh861@gmail.com>
2023-12-25 01:28:09 +0530

    setup done along with some frontend part and api

---
+++ extension/popup.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById('summarize-btn');
+btn.addEventListener('click', () => {
+    btn.disabled = true;
+    btn.innerHTML = 'Summarizing...';
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var url = tabs[0].url;
+        var hxr = new XMLHttpRequest();
+        hxr.open("GET", "http://127.0.0.1:5000/summary?url=" + url, true);
+        hxr.onload = function () {
+            var text = hxr.responseText;
+            var summary = document.getElementById('summary');
+            summary.innerHTML = text;
+            btn.disabled = false;
+            btn.innerHTML = 'Summarize';
+        }
+        hxr.send();
+    });
+});
+
